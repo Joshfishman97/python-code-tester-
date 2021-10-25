d1ecac410a4fad4c6b0615d14944b3c4064ec45a
Joshfishman97 <fishmanjosh251@gmail.com>
2021-10-25 22:57:09 +0000

    another commit created some functions for this game and uploaded some files

---
+++ .github/workflows/game.py
@@ -0,0 +1,30 @@
+from random import shuffle  
+example = [1,2,3,4,5,6,7]
+shuffle(example)
+example
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
+
+result = shuffle_list(example)
+result
+
+mylist = ['O', '', '']
+
+shuffle_list(mylist)
+
+def player_guess():
+    guess = ''
+    while guess not in ['0','1','2']:
+        guess = input("Pick a number:0,1,2")
+        
+    return int(guess)
+
+player_guess()
+
+def check_guess(mylist, guess):
+    if mylist[guess] == 'O':
+        print("correct")
+    else:
+        print("Wrong guess!")
